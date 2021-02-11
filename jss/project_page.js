4746d1064fc3e662e7b006929527cbe21e2b7d75
Miralllll <55250021+Miralllll@users.noreply.github.com>
2021-02-12 02:49:17 +0400

    project page improvement

--- jss/project_page.js
+++ jss/project_page.js
@@ -31,6 +31,13 @@ async function getProjectInfo(id) {
     "$" + jsonRes.project.remaining;
   document.querySelector("div.filled").style.width =
     (jsonRes.project.funding / jsonRes.project.goal) * 100 + "%";
+  document.querySelector("p.summary").innerHTML = jsonRes.project.summary;
+  console.log(jsonRes.project.activites);
+  document.querySelector("p.challenge").innerHTML = jsonRes.project.need;
+  document.querySelector("p.solution").innerHTML = jsonRes.project.activities;
+  document.querySelector("p.impact").innerHTML = jsonRes.project.longTermImpact;
+  document.querySelector("div.project-container").querySelector("a").href =
+    jsonRes.project.additionalDocumentation;
 }
 
 var index = 0;
@@ -116,7 +123,19 @@ async function displaySummary(id) {
   var parser = new DOMParser();
   var xml = parser.parseFromString(result, "text/xml");
   const jsonRes = converterXMLToJson(xml);
+  console.log(jsonRes.project);
 }
+
+checkElement("a.icon", function () {
+  document.querySelector("a.icon").addEventListener("click", function () {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "proj-nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "proj-nav";
+    }
+  });
+});
 
 function projectDisplay(id) {
   const project_html = `
@@ -147,34 +166,59 @@ function projectDisplay(id) {
                     </div>
                 </div>
             </div>
-            <div class="image-donate">
-                <div class="slider">
-                    <div class="image-slider">
-                        <div class="slider-items">
-                            <div class="curr-active">
-                                <div class="img-some"><img src=""/></div>
-                            </div>
-                        </div>
-                        <div class="left-slide">&lt;</div>
-                        <div class="right-slide">&gt;</div>
-                    </div>
+            <div class="wrapper-image-donate">
+              <div class="image-donate">
+                  <div class="slider">
+                      <div class="image-slider">
+                          <div class="slider-items">
+                              <div class="curr-active">
+                                  <div class="img-some"><img src=""/></div>
+                              </div>
+                          </div>
+                          <div class="left-slide">&lt;</div>
+                          <div class="right-slide">&gt;</div>
+                      </div>
+                  </div>
+                  <div class="donate">
+                      <div class="collected"> <span class="collected-num"> </span>  raised of  <span class="goal-num"> </span>  goal</div>
+                      <div class="percent-bar">
+                          <div class="filled"> </div>
+                      </div>
+                      <div>
+                          <div class="donation-num"> <span> </span> donations </div>
+                          <div class="stayed-money"> <span> </span> to go </div>
+                      </div>
+                      <div id="join">
+                          <button class="new-some button-white-indigo font donateBtn">Donate Once</button>
+                      </div>
+                      <div id="join">
+                          <button class="new-some button-white-indigo font donateBtn">Donate Monthly</button>
+                      </div>
+                  </div>
+              </div>
+            </div>
+            <div class="wrapper-proj-nav">
+                <div class="proj-nav" id="myTopnav">
+                  <a href="#story" class="story">STORY</a>
+                  <a href="#reports">REPORTS</a>
+                  <a href="#photos">PHOTOS</a>
+                  <a href="#share">SHARE</a>
+                  <a href="javascript:void(0);" class="icon">
+                    <i class="fa fa-bars" style="margin: 0; padding: 0; font-size: 22px;"></i>
+                  </a>
                 </div>
-                <div class="donate">
-                    <div class="collected"> <span class="collected-num"> </span>  raised of  <span class="goal-num"> </span>  goal</div>
-                    <div class="percent-bar">
-                        <div class="filled"> </div>
-                    </div>
-                    <div>
-                        <div class="donation-num"> <span> </span> donations </div>
-                        <div class="stayed-money"> <span> </span> to go </div>
-                    </div>
-                    <div id="join">
-                        <button class="new-some button-white-indigo font donateBtn">Donate Once</button>
-                    </div>
-                    <div id="join">
-                        <button class="new-some button-white-indigo font donateBtn">Donate Monthly</button>
-                    </div>
-                </div>
+            </div>
+            <div class="project-container">
+              <p class="pr-title"> Summary </p>
+              <p class="summary text"> </p>
+              <p class="pr-title"> Challenge </p>
+              <p class="challenge text"> </p>
+              <p class="pr-title"> Solution </p>
+              <p class="solution text"> </p>
+              <p class="pr-title"> Long-Term Impact </p>
+              <p class="impact text"> </p>
+              <p class="pr-title"> Additional Documentation </p>
+              <a class="text" href="#document"> This project has provided additional documentation in a PDF file (projdoc.pdf). <a>
             </div>
         </div>
     `;
